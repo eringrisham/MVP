b124865cc7e145423d5436990ec042e682512ea2
Erin Grisham <erinmgrisham@gmail.com>
2021-08-24 11:29:51 -0400

    dog cards rendering!!

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -3,13 +3,16 @@ import axios from 'axios';
 import './app.css';
 import BackgroundImage from '../../../images/1405992047-vector.svg';
 import DogLogo from '../../../images/shutterstock_1518711302.svg'
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { Container, AppBar, Typography, Grow, Grid, InputBase } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 import useStyles from './styles';
+import DogCards from './dogCards/dogCards.jsx';
 
 const App = () => {
 	const classes = useStyles();
 
 	let [dogs, fetchAllDogs] = useState();
+
 
 	useEffect(async () => {
 		const result = await axios(
@@ -17,6 +20,7 @@ const App = () => {
 		);
 		fetchAllDogs(result.data);
 	}, []);
+
 
 	//axios.get('http://localhost:3014/dogs').then(data => {
 
@@ -29,19 +33,39 @@ const App = () => {
       <AppBar className={classes.appBar} position='static' color='inherit'>
 				<Typography className={classes.heading} variant='h2' align='center'>Pawsome</Typography>
 				<img className={classes.image} src={DogLogo} alt='memories' height='60'/>
+				<div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
 			</AppBar>
-			<div className='Body'>
+			{/* <div className='Body'>
 	 		<img src={BackgroundImage} alt='Paw-print background image'/>
-	  	</div>
+	  	</div> */}
 			<Grow in>
 				<Container>
-					<Grid className={classes.mainContainer} container justify='space-between' alignItems='stretch' spacing={3}>
-						<Grid item xs={12} sm={7}>
-              {/* <Posts setCurrentID={setCurrentID} /> */}
-						</Grid>
-						<Grid item xs={12} sm={4}>
+					<Grid className={classes.mainContainer} container justifyContent='space-between' alignItems='stretch' spacing={3}>
+
+							{dogs ?
+							dogs.map((dog, i) => (
+								dog.name !== 'African Hunting Dog' ?
+								<Grid item xs={4} >
+								<DogCards key={i} dog={dog}/>
+								</Grid> : null
+							))
+							: null}
+
+						{/* <Grid item xs={12} sm={4}> */}
 							{/* <Form currentID={currentID} setCurrentID={setCurrentID} /> */}
-						</Grid>
+						{/* </Grid> */}
 					</Grid>
 
 				</Container>
---
+++ client/src/components/dogCards/dogCards.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import useStyles from './styles.js';
+import { Card, CardHeader, CardMedia, CardContent, CardActions, Collapse, IconButton, Typography } from '@material-ui/core';
+
+import clsx from 'clsx';
+
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+const DogCards = ({ dog }) => {
+	const classes = useStyles();
+	const [expanded, setExpanded] = useState(false);
+
+	const [favorites, setFavorites] = useState();
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        action={
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+        }
+        title={dog.name}
+        subheader={dog.breed_group}
+				titleTypographyProps={{variant:'h5'}}
+      />
+      <CardMedia
+        className={classes.media}
+        image={dog.image_url}
+        title="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          Temperament: {dog.temperament}
+					<br/>
+					Weight: {dog.weight} lb.
+					<br/>
+          Height: {dog.height} in.
+
+
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Notes:</Typography>
+          <Typography paragraph>
+            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+            minutes.
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
+
+export default DogCards;
